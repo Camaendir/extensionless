9a7cb1b7adbb6b0b19a24663cff19e0245258b3e
Camaendir <dev@camaendir.de>
2025-12-18 17:25:59 +0100

    remove styles.css, add settings tab, fix typescript warnings

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Plugin, TAbstractFile } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, TAbstractFile } from 'obsidian';
 
 interface ExtensionlessSettings {
 	enable: boolean;
@@ -14,70 +14,85 @@ const DEFAULT_SETTINGS: ExtensionlessSettings = {
 export default class ExtensionlessPlugin extends Plugin {
 	settings: ExtensionlessSettings = DEFAULT_SETTINGS;
 
-	overwrite_getLinkpathDest(link: string, source: string){
+	basename(path: string) {
+		const t = path.lastIndexOf("/");
+		return -1 === t ? path : path.slice(t + 1);
+	}
 
-		var basename = (path: string) => {
-            var t = path.lastIndexOf("/");
-            return -1 === t ? path : path.slice(t + 1);
-		};
+	dirname(path: string) {
+		const t = path.lastIndexOf("/");
+		return -1 === t ? "" : path.slice(0, t);
+	}
 
-		var	dirname = (path: string) => {
-			var t = path.lastIndexOf("/");
-			return -1 === t ? "" : path.slice(0, t);
-		};
+	comparison_function(file1: TAbstractFile, file2: TAbstractFile) {
+		return file1.path.length - file2.path.length;
+	}
 
-		var comparison_function = (file1: TAbstractFile, file2: TAbstractFile) => {
-			return file1.path.length - file2.path.length;
-		}
+	mygetLinkpathDest(link:string, sourceFile: string) {
+		let f;
+		if ("" === link && sourceFile && (f = this.app.vault.getAbstractFileByPath(sourceFile)) !== null)
+			return [f];
+		let workingLink = link.toLowerCase();
+		const tmpFileLink = this.basename(workingLink);
 
-		var mygetLinkpathDest = (link:string, sourceFile: string) => {
-			if ("" === link && sourceFile && (f = this.app.vault.getAbstractFileByPath(sourceFile)) !== null)
-				return [f];
-			var workingLink = link.toLowerCase();
-			var tmpFileLink = basename(workingLink);
+		//@ts-ignore
+		const fileResult = this.app.metadataCache.uniqueFileLookup.get(tmpFileLink);
+		
+		if (!fileResult)
+			return [];
 
-			//@ts-ignore
-			var fileResult = this.app.metadataCache.uniqueFileLookup.get(tmpFileLink);
-			
-			if (!fileResult)
-				return [];
-
-			if (tmpFileLink === workingLink && 1 === fileResult.length)
-				return fileResult.slice();
-			var sourceDirName = dirname(sourceFile).toLowerCase();
-			if (workingLink.startsWith("./") || workingLink.startsWith("../")) {
-				if (workingLink.startsWith("./../") && (workingLink = workingLink.substr(2)),
-				workingLink.startsWith("./"))
-					"" !== sourceDirName && (sourceDirName += "/"),
-					workingLink = sourceDirName + workingLink.substring(2);
-				else {
-					for (; workingLink.startsWith("../"); )
-						workingLink = workingLink.substr(3),
-						sourceDirName = dirname(sourceDirName);
-					"" !== sourceDirName && (sourceDirName += "/"),
-					workingLink = sourceDirName + workingLink
+		if (tmpFileLink === workingLink && 1 === fileResult.length)
+			return fileResult.slice();
+		let sourceDirName = this.dirname(sourceFile).toLowerCase();
+		let m;
+		if (workingLink.startsWith("./") || workingLink.startsWith("../")) {
+			if (workingLink.startsWith("./../") && (workingLink = workingLink.slice(2)), workingLink.startsWith("./")){
+				if ("" === sourceDirName) {
+					sourceDirName += "/"
 				}
-				for (var a = 0, s = fileResult; a < s.length; a++) {
-					if ((m = (f = s[a]).path.toLowerCase()) === workingLink)
-						return [f]
-				}
+				workingLink = sourceDirName + workingLink.substring(2);
 			}
-			workingLink.startsWith("/") && (workingLink = workingLink.substr(1));
-			for (var l = 0, c = fileResult; l < c.length; l++) {
-				if ((m = (f = c[l]).path.toLowerCase()) === workingLink)
+				
+			else {
+				for (; workingLink.startsWith("../"); ){
+					workingLink = workingLink.slice(3);
+					sourceDirName = this.dirname(sourceDirName);
+				}
+				if ("" === sourceDirName){
+					sourceDirName += "/";
+				}
+				workingLink = sourceDirName + workingLink;
+			}
+			let a = 0, s = fileResult;
+			for (; a < s.length; a++) {
+				if ((m = (f = s[a]).path.toLowerCase()) === workingLink)
 					return [f]
 			}
-			if (link.startsWith("/"))
-				return [];
-			for (var u = [], h = [], p = 0, d = fileResult; p < d.length; p++) {
-				var f, m;
-				(m = (f = d[p]).path.toLowerCase()).endsWith(workingLink) && (m.startsWith(sourceDirName) ? u.push(f) : h.push(f))
-			}
-			return u.sort(comparison_function),
-			h.sort(comparison_function),
-			u.concat(h)
 		}
+		if (!workingLink.startsWith("/")){
+			workingLink = workingLink.slice(1);
+		}
+		let l = 0, c = fileResult
+		for (; l < c.length; l++) {
+			if ((m = (f = c[l]).path.toLowerCase()) === workingLink)
+				return [f]
+		}
+		if (link.startsWith("/"))
+			return [];
+		let u = [], h = [], p = 0, d = fileResult;
+		for (; p < d.length; p++) {
+			let f  = d[p]
+			let m  = f.path.toLowerCase()
+			if (!m.endsWith(workingLink)) {
+				m.startsWith(sourceDirName) ? u.push(f) : h.push(f)
+			}
+		}
+		return u.sort(this.comparison_function),
+		h.sort(this.comparison_function),
+		u.concat(h)
+	}
 
+	overwrite_getLinkpathDest(link: string, source: string){
 
 		if (this.settings === undefined){
 			this.settings = DEFAULT_SETTINGS;
@@ -95,13 +110,13 @@ export default class ExtensionlessPlugin extends Plugin {
 			const original_result = this.app.metadataCache.getLinkpathDest_old(link, source);
 
 			if (!Array.isArray(original_result) || original_result.length == 0){
-				return mygetLinkpathDest(link, source);
+				return this.mygetLinkpathDest(link, source);
 			}
 			return original_result;
 
 		}else{
 
-			const my_result = mygetLinkpathDest(link, source);
+			const my_result = this.mygetLinkpathDest(link, source);
 
 			if (!Array.isArray(my_result) || my_result.length == 0){
 				// see above
@@ -115,6 +130,7 @@ export default class ExtensionlessPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
+		this.addSettingTab(new ExtensionlessSettingsTab(this.app, this));
 
 		// see above
 		//@ts-ignore
@@ -122,7 +138,7 @@ export default class ExtensionlessPlugin extends Plugin {
 
 		// see above
 		//@ts-ignore
-		this.app.metadataCache.getLinkpathDest = this.overwrite_getLinkpathDest;
+		this.app.metadataCache.getLinkpathDest = this.overwrite_getLinkpathDest.bind(this);
 	}
 
 	onunload() {
@@ -138,4 +154,46 @@ export default class ExtensionlessPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+}
+
+class ExtensionlessSettingsTab extends PluginSettingTab {
+    plugin: ExtensionlessPlugin;
+
+    constructor(app: App, plugin: ExtensionlessPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+    display() {
+        const { containerEl } = this;
+        containerEl.empty();
+
+        const header = containerEl.createEl('h2', {
+            text: 'Extensionless Settings'
+        });
+
+        new Setting(containerEl)
+            .setName('Enable Extensionless Links')
+            .setDesc('Enable or disable the link resolver overwrite.')
+            .addToggle(toggle => 
+                toggle
+                    .setValue(this.plugin.settings.enable)
+                    .onChange(async (value) => {
+                        this.plugin.settings.enable = value;
+                        await this.plugin.saveSettings();
+                    })
+            );
+
+        new Setting(containerEl)
+            .setName('Find Markdown First')
+            .setDesc('Set if an extensionless link should search for a markdown file first. This is typically faster for normal links, but you cannot create a link to an extensionless file when there is a markdown file with the same name in the same folder. Requires reload to fully take effect.')
+            .addToggle(toggle => 
+                toggle
+                    .setValue(this.plugin.settings.markdownFirst)
+                    .onChange(async (value) => {
+                        this.plugin.settings.markdownFirst = value;
+                        await this.plugin.saveSettings();
+                    })
+            );
+    }
 }
